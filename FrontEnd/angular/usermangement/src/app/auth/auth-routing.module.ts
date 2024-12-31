97cacac0b7f2b74e0c5bf0b2c2800834aa1cb602
Ramakrishnan Ramanarayanan <rramanarayanan@explorance.com>
2024-12-31 14:55:55 +0530

    router issue resolving

--- FrontEnd/angular/usermangement/src/app/auth/auth-routing.module.ts
+++ FrontEnd/angular/usermangement/src/app/auth/auth-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'forgotpassword', component: ForgotPasswordComponent },
+  { path: 'resetpassword', component: ResetPasswordComponent },
   //Wild Card Route for 404 request 
   { path: '**', pathMatch: 'full', redirectTo: '/pagenotfound' },
 
